0b8ca0297b573f75261cbc3a8c807938779ab7d3
Christian Vogt <mail@christianvogt.de>
2014-09-23 13:01:54 +0200

    Ping protocol can now handle multiple requests
    * Added a seq. number to make sure received pong messages can be mapped
    to the correct ping.
    * Added a timeout mechanism
    * Adapted GUI

--- current/protocols.ping.js
+++ current/protocols.ping.js
@@ -5,7 +5,7 @@ Ping = function(bopclient) {
     this._bopclient = bopclient;
     this._protocolName = 'ping-protocol';
     this._bopclient.setOnMessageHandler(this._protocolName, this._onMessage.bind(this));
-    this._timer = null;
+    this._pendingPings = {};
     return this;
 };
 
@@ -14,18 +14,36 @@ Ping.prototype = {
 		if (msg.type === 'ping') {
 			this._bopclient.send(from, this._protocolName, {
 				date: Date(),
-				type: 'pong'
+				type: 'pong',
+				ref: msg.ref
 			});
 		} else if (msg.type ==='pong') {
-			var diff = Math.abs(this._timer - new Date());
-			console.log('pinging ' + from + ' took ' + diff + 'ms.');
+			var ref = msg.ref;
+			if (this._pendingPings[ref]) {
+				var diff = Math.abs(new Date() - this._pendingPings[ref].date);
+				try {
+					this._pendingPings[ref].cb(null, diff);
+				} catch(e) {}
+				delete this._pendingPings[ref];
+			}
 		}
 	},
-	sendPing: function(to) {
-		this._timer = new Date();
-		this._bopclient.send(to, this._protocolName, {
-			date: Date(),
-			type: 'ping'
-		});
+	sendPing: function(to, timeout, cb) {
+		var ref = Math.floor((Math.random() * 100000) +1);
+		this._pendingPings[ref] = {
+			date: new Date(),
+			type: 'ping',
+			ref: ref,
+			cb: cb
+		};
+		if (typeof(timeout) === 'number') {
+			setTimeout(function() {
+				if (this._pendingPings[ref]) {
+					delete this._pendingPings[ref];
+					cb(to + ' timed out');
+				}
+			}.bind(this), timeout);
+		}
+		this._bopclient.send(to, this._protocolName, this._pendingPings[ref]);
 	}
 };
